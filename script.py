0b3061e4513ae9af50e5d13bac337c0707207539
Vahid Al <thevahidal@gmail.com>
2023-12-28 21:30:59 +0330

    Improve styling

--- script.py
+++ script.py
@@ -7,15 +7,21 @@ with open("data.toml", "rb") as f:
     data = tomllib.load(f)
     sections = frag(
         h("div", klass="section")(
-            h("h1", klass="title")(section.get("title")),
-            h("p", klass="description")(section.get("description")),
+            h("hgroup")(
+                h("h3")(section.get("title")),
+                h("p")(section.get("description")),
+            ),
             (
                 h("div", klass="item")(
-                    h("h2", klass="title")(item.get("title")),
-                    h("p", klass="description")(item.get("description"))
-                    if item.get("description")
-                    else None,
-                    h("a", klass="url", href=item.get("url"))("Link"),
+                    h("a", href=item.get("url"), target="_blank")(
+                        h("hgroup")(
+                            h("h4")(item.get("title")),
+                            h("p")(item.get("description"))
+                            if item.get("description")
+                            else None,
+                            "",
+                        ),
+                    ),
                 )
                 for item in section["items"]
             ),
@@ -23,24 +29,55 @@ with open("data.toml", "rb") as f:
         for section in data["sections"]
     )
 
-    output = html(lang="en")(
+    head = frag(
         h("head")(
+            h("title")(data.get("name")),
+            h("meta", name="description", content=data.get("description")),
+            h("meta", name="keywords", content=data.get("keywords")),
+            h("meta", name="viewport", content="width=device-width, initial-scale=1"),
             h("meta", charset="utf-8"),
-            h("style")(
-                raw(
-                    """
-              body {
-               font-family: sans-serif;
-              }
-              .section > .title {
-                 color: red;
-              }
-            """
-                )
-            ),
+            h("link", rel="stylesheet", href="css/pico.min.css"),
+            h("link", rel="stylesheet", href="css/style.css"),
         ),
-        h("body")(h("h1")(data.get("name")), h("p")(data.get("bio")), sections),
+    )
+
+    header = frag(
+        h("header", klass="container")(
+            h("hgroup")(
+                h(
+                    "img",
+                    klass="avatar",
+                    src=f"img/{data.get('image')}",
+                    alt="avatar",
+                ),
+                h("h1")(data.get("name")),
+                h("p")(data.get("description")) if data.get("description") else None,
+            ),
+        )
+    )
+
+    footer = frag(
+        h("footer", klass="container")(
+            h("small")("Generated with "),
+            h(
+                "a",
+                klass="",
+                href="https://github.com/thevahidal/jake/",
+                target="_blank",
+            )("Jake"),
+        ),
+    )
+
+    output = html(lang="en")(
+        head,
+        h("body")(
+            header,
+            h("main", klass="container")(
+                sections,
+            ),
+            footer,
+        ),
     ).render()
 
-    with open("index.html", "w") as f:
+    with open("dist/index.html", "w") as f:
         f.write(output)
