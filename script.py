2670fccc5fcd166e38e28863863fa742a873d32e
Vahid Al <thevahidal@gmail.com>
2023-12-28 16:46:57 +0330

    Add site generator

--- script.py
+++ script.py
@@ -1,4 +1,46 @@
-import requests
-print("Hellow!")
-response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-print(response.json())
+import tomllib
+
+from tinyhtml import html, h, frag, raw
+
+
+with open("data.toml", "rb") as f:
+    data = tomllib.load(f)
+    sections = frag(
+        h("div", klass="section")(
+            h("h1", klass="title")(section.get("title")),
+            h("p", klass="description")(section.get("description")),
+            (
+                h("div", klass="item")(
+                    h("h2", klass="title")(item.get("title")),
+                    h("p", klass="description")(item.get("description"))
+                    if item.get("description")
+                    else None,
+                    h("a", klass="url", href=item.get("url"))("Link"),
+                )
+                for item in section["items"]
+            ),
+        )
+        for section in data["sections"]
+    )
+
+    output = html(lang="en")(
+        h("head")(
+            h("meta", charset="utf-8"),
+            h("style")(
+                raw(
+                    """
+              body {
+               font-family: sans-serif;
+              }
+              .section > .title {
+                 color: red;
+              }
+            """
+                )
+            ),
+        ),
+        h("body")(sections),
+    ).render()
+
+    with open("output.html", "w") as f:
+        f.write(output)
